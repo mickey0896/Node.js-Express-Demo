d921810b089b6f888e36708d04cc8b6bcc3bc679
mickey0896 <mickey_0896@hotmail.com>
2024-08-25 13:33:32 +0700

    'commit'

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Listening on port" + chalk.green(" : ", port));
+  debug("Listening on port" + chalk.green(" : ", port));
 });
